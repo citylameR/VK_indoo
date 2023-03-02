6aa43fee58f7cf7fd0b94d8dc637cd7de3a40f6d
David <dvdchernichenko@gmail.com>
2023-03-02 17:01:30 +0300

    Initial commit

--- db/quick_commands.py
+++ db/quick_commands.py
@@ -1,8 +1,7 @@
 from asyncpg import UniqueViolationError
 
 from db.dp_gino import db
-from db.schemas.user import User, Favorites
-
+from db.schemas.user import User, Favorite_person, Favorites, Offer
 
 
 async def add_user(user_id: int, first_name: str, last_name: str, city: int, age: int, age_min: int,
@@ -20,14 +19,6 @@ async def add_user(user_id: int, first_name: str, last_name: str, city: int, age
         print('Пользователь не добавлен')
 
 
-async def add_fav(user_id: int, fav_id: int):
-    try:
-        favorite = Favorites(user_id=user_id, favorite_id=fav_id)
-        await favorite.create()
-    except UniqueViolationError:
-        print('Пользователь не добавлен')
-
-
 
 async def select_user(user_id):
 
@@ -36,19 +27,22 @@ async def select_user(user_id):
     """
 
     user = await User.query.where(User.user_id == user_id).gino.first()
-    if user == None:
-        return None
-    else:
-        usr = {}
-        usr['id'] = user.user_id
-        usr['first_name'] = user.first_name
-        usr['last_name'] = user.last_name
-        usr['city'] = user.city
-        usr['sex'] = user.sex
-        usr['age'] = user.age
-        usr['age_min'] = user.age_min
-        usr['age_max'] = user.age_max
-        return usr
+    if user == True:
+        print('Пользователь есть')
+
+async def add_favorites_person(user_id, favorites_id, url):
+    """
+    Функция принимает на вход user_id, favorite_id, и url пользователя, которого лайкнули и добавляет его в БД
+    """
+
+    user_favorite = await db.scalar(db.exists().where(Favorite_person.user_id == user_id and Favorite_person.favorites_id == favorites_id).select())
+    if user_favorite is False:
+        favorite_person = Favorite_person(user_id=user_id, favorites_id=favorites_id)
+        favorite = Favorites(favorites_id=favorites_id, url=url)
+        await favorite.create()
+        await favorite_person.create()
+
+        return True
 
 
 async def list_favorites(user_id):
@@ -56,17 +50,25 @@ async def list_favorites(user_id):
     Функция принимает user_id и возвращает список избранных пользователей
     """
 
-    user = await Favorites.query.where(Favorites.user_id == user_id).gino.all()
+    user = await Favorite_person.query.where(Favorite_person.user_id == user_id).gino.all()
     favorites_list = []
     for i in user:
-        favorites_list.append(i.favorite_id)
+        favorites_list.append(i)
+
     return favorites_list
 
 
-# async def delete_favorites(user_id):
-#     """
-#     Функция принимает user_id и удаляет избранного пользователя
-#     """
-#
-#     favorite = await  Favorite_person.query.where(Favorite_person.user_id == user_id).gino.all()
-#     await favorite.delete()
+async def delete_favorites(user_id):
+    """
+    Функция принимает user_id и удаляет избранного пользователя
+    """
+
+    favorite = await  Favorite_person.query.where(Favorite_person.user_id == user_id).gino.all()
+    await favorite.delete()
+
+async def add_offer(offered_id, user_id):
+    user = await db.scalar(db.exists().where(Offer.user_id == user_id and Offer.offer_id == offered_id).select())
+    if user is False:
+        offered = Offer(offer_id=offered_id, user_id=user_id)
+        await offered.create()
+        return 'added'
